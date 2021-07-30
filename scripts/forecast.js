b1ee0959a31ad91e72433bf80c11797d23109d64
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-07-31 00:02:04 +0700

    Initial commit

---
+++ scripts/forecast.js
@@ -0,0 +1,34 @@
+const key = 'QL3wcEuUV0QsGVLWLKEnRup1pFI26VAR'
+
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+}
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+}
+
+// getCity('Jakarta')
+//     .then((data) => {
+//         console.log(data)
+//         return getWeather(data.Key)
+//     })
+//     .then((data) => {
+//         console.log(data)
+//     })
+//     .catch(err => console.log(err))
+
